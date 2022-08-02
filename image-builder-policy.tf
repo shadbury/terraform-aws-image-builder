805d899be6dff734c29ec702571a9c301d489317
Joel.Hutson <joel.hutson@cmd.com.au>
2022-08-02 18:59:30 +1000

    create log bucket

--- image-builder-policy.tf
+++ image-builder-policy.tf
@@ -47,7 +47,7 @@ data "aws_iam_policy_document" "image_builder" {
     actions = [
       "s3:PutObject"
     ]
-    resources = ["arn:aws:s3:::${module.s3.aws_s3_bucket.logging_bucket[0].id}/image-builder/*"]
+    resources = ["arn:aws:s3:::${var.image_builder_aws_s3_bucket}-logging/image-builder/*"]
   }
 
   statement {
@@ -84,7 +84,4 @@ data "aws_iam_policy_document" "image_builder" {
       ]
     }
   }
-  depends_on = [
-    module.s3.aws_s3_bucket.logging_bucket[0]
-  ]
 }
