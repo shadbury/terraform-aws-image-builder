909b41582e78f9ce8bd9d18e95478015baebbb48
Joel.Hutson <joel.hutson@cmd.com.au>
2022-08-02 18:23:47 +1000

    updating SG

--- components.tf
+++ components.tf
@@ -33,15 +33,14 @@ resource "aws_imagebuilder_component" "cw_agent" {
 resource "aws_security_group" "image-builder" {
   name        = "image-builder"
   description = "image-builder"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
@@ -49,6 +48,5 @@ resource "aws_security_group" "image-builder" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 }
